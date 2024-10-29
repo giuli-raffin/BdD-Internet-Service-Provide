cd6dcbb9b26b4b4c866aef2ae196685edf907e72
Giuliana Raffin <raffingiuliana@gmail.com>
2024-10-29 16:37:45 -0300

    Update 1-Consultas.sql

--- 1-Consultas.sql
+++ 1-Consultas.sql
@@ -1,6 +1,7 @@
 1. Consultas
 
---a
+--a. Mostrar el listado de todos los clientes registrados en el sistema (id, apellido y nombre, tipo y número de documento, fecha de nacimiento)
+    -- junto con la cantidad de equipos registrados que cada uno dispone, ordenado por apellido y nombre.
 select cl.id_cliente, apellido, p.nombre, tipodoc, nrodoc, fecha_nacimiento, count(*)
 from cliente cl
 join (select id_persona, nombre, apellido, tipodoc, nrodoc, fecha_nacimiento from persona) p on cl.id_cliente = p.id_persona
@@ -8,7 +9,8 @@ join (select id_cliente from equipo) e on cl.id_cliente = e.id_cliente
 group by cl.id_cliente, apellido, p.nombre, tipodoc, nrodoc, fecha_nacimiento
 order by apellido, p.nombre;
 
---b
+--b. Realizar un ranking (de mayor a menor) de la cantidad de equipos instalados y aún activos, durante los últimos 24 meses, según su distribución
+    -- geográfica, mostrando: nombre de ciudad, id de la ciudad, nombre del barrio, id del barrio y cantidad de equipos.
 select ci.nombre as ciudad, ci.id_ciudad, b.nombre as barrio, b.id_barrio, count(e.id_equipo) as cant_equipos
 from ciudad ci
 join (select id_ciudad, id_barrio, nombre from barrio) b on ci.id_ciudad = b.id_ciudad
@@ -23,7 +25,7 @@ where e.fecha_baja is null and ((extract(year from current_timestamp) - extract(
 group by ci.nombre, ci.id_ciudad, b.nombre, b.id_barrio
 order by count(e.id_equipo) desc;
 
---c
+--c. Visualizar el Top-3 de los lugares donde se ha realizado la mayor cantidad de servicios periódicos durante los últimos 3 años.
 select d.id_direccion, calle, numero, piso, depto, id_barrio
 from direccion d
 join (select id_cliente from cliente) c on d.id_persona = c.id_cliente
@@ -35,7 +37,7 @@ group by d.id_direccion, calle, numero, piso, depto, id_barrio
 order by count(*) desc
 limit 3;
 
---d
+--d. Indicar el nombre, apellido, tipo y número de documento de los clientes que han contratado todos los servicios periódicos cuyo intervalo se encuentra entre 5 y 10.
 select p.nombre, apellido, tipodoc, nrodoc
 from cliente cl
 join (select id_persona,nombre,apellido,tipodoc,nrodoc from persona) p on cl.id_cliente = p.id_persona
